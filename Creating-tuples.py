3d7544b89828b5a55a48436c241f93f221856f86
Shaik Mahaboob Basha <shaikmahaboob3247@gmail.com>
2024-09-16 17:57:19 +0530

    Create Creating-tuples.py

---
+++ Creating-tuples.py
@@ -0,0 +1,8 @@
+# Creating tuples
+empty_tuple = ()
+single_element_tuple = (42,)
+multi_element_tuple = (1, "devops", 3.14)
+
+print(empty_tuple)
+print(single_element_tuple)
+print(multi_element_tuple)
