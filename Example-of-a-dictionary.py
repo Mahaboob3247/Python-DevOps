d9141b9fed31d8b1622fea4a3ec8a622ef32afc7
Shaik Mahaboob Basha <shaikmahaboob3247@gmail.com>
2024-09-16 17:55:17 +0530

    Create Example-of-a-dictionary.py

---
+++ Example-of-a-dictionary.py
@@ -0,0 +1,13 @@
+# Example of a dictionary
+server_info = {
+    "name": "Server1",
+    "IP": "192.168.1.1",
+    "status": "active"
+}
+
+# Accessing dictionary items
+print(server_info["name"])
+
+# Modifying dictionary items
+server_info["status"] = "inactive"
+print(server_info)
